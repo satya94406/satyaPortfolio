c917638523b9a11fc0d0eb870f9c4025096e72f8
satya960 <satya9604166@gmail.com>
2024-10-10 11:26:12 +0530

    some lines updated

--- src/components/About.js
+++ src/components/About.js
@@ -8,15 +8,21 @@ const About = () => {
             About Me
           </h1>
           <ul className=" text-lg text-zinc-600 md:text-xl md:px-10 pb-2 ml-2 ">
-            <li className="pb-2 ml-2 animate-fadeIn delay-[1000ms]">🚀  I am currently pursuing my B.Tech in Information Technology (2nd Year).</li>
-            <li className="pb-2 ml-2 animate-fadeIn delay-[2000ms]">🚀  My college is Maharaja Surajmal Institute of Technology.</li>
-            <li className="pb-2 ml-2 animate-fadeIn delay-[3000ms]">🚀  I have completed my Diploma in Electrical Engineering from GNDIT college.</li>
-            <li className="pb-2 ml-2 animate-fadeIn delay-[4000ms]">🚀  I have 3 years of experience at a 66/33/11 KV substation in Tata Power DDL.</li>
-            <li className="pb-2 ml-2 animate-fadeIn delay-[5000ms]">🚀  Currently, I am a web developer focused on frontend technologies.</li>
-            <li className="pb-2 ml-2 animate-fadeIn delay-[6000ms]">🚀  I have joined the Microsoft Chapter Student (MSC) society.</li>
-            <li className="pb-2 ml-2 animate-fadeIn delay-[7000ms]">🚀  I am passionate about learning new technologies and skills.</li>
-            <li className="pb-2 ml-2 animate-fadeIn delay-[8000ms]">🚀  I am keen to explore AI/ML-related skills.</li>
-            <li className="pb-4 ml-2 animate-fadeIn delay-[9000ms]">🚀  I am also a member of the Geek Room in the Department of DSA.</li>
+            <li className="pb-2 ml-2 animate-fadeIn delay-[1000ms]">🚀 I am currently pursuing my B.Tech in Information Technology (2nd Year) at Maharaja Surajmal Institute of Technology.</li>
+            <li className="pb-2 ml-2 animate-fadeIn delay-[2000ms]">🚀 I have completed my Diploma from Guru Nanak Dev Institute.</li>
+            <li className="pb-2 ml-2 animate-fadeIn delay-[3000ms]">🚀 I have acquired skills in HTML, CSS, JavaScript, and React.js.</li>
+            <li className="pb-2 ml-2 animate-fadeIn delay-[4000ms]">🚀 Currently, I am a web developer focused on frontend technologies.</li>
+            <li className="pb-2 ml-2 animate-fadeIn delay-[5000ms]">🚀 I am a proud member of the Microsoft Student Chapter (MSC) society.</li>
+            <li className="pb-2 ml-2 animate-fadeIn delay-[6000ms]">🚀 I am passionate about learning new technologies and improving my skills.</li>
+            <li className="pb-2 ml-2 animate-fadeIn delay-[7000ms]">🚀 I am passionate about learning new technologies and skills.</li>
+            <li className="pb-2 ml-2 animate-fadeIn delay-[8000ms]">🚀 I am keen to explore AI/ML-related technologies.</li>
+            <li className="pb-4 ml-2 animate-fadeIn delay-[9000ms]">🚀 I am actively working on projects like a Swiggy clone, a weather app, and a YouTube clone to sharpen my development skills.</li>
+            <li className="pb-4 ml-2 animate-fadeIn delay-[9000ms]">🚀 I am proficient in using tools like GitHub for version control and deploying web applications.</li>
+            <li className="pb-4 ml-2 animate-fadeIn delay-[9000ms]">🚀 I have a strong foundation in data structures and algorithms, which I continue to improve as part of the Geek Room community.</li>
+            <li className="pb-4 ml-2 animate-fadeIn delay-[9000ms]">🚀 In addition to frontend development, I am exploring backend technologies to become a full-stack developer.</li>
+            <li className="pb-4 ml-2 animate-fadeIn delay-[9000ms]">🚀 I enjoy participating in hackathons and coding challenges to test my skills in real-world scenarios.</li>
+            <li className="pb-4 ml-2 animate-fadeIn delay-[9000ms]">🚀 My portfolio is available on GitHub Pages, where I showcase the projects I have worked on.</li>
+
           </ul>
         <Footer />
       </div>
