1f698f398c4a043ffd00f2888d53b68f5d65d504
satya960 <satya9604166@gmail.com>
2024-10-17 02:27:11 +0530

    Initial commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,24 +2,48 @@ import { useEffect, useState } from "react";
 import photo from "../Assests/images/photo.jpeg";
 import Footer from "./Footer";
 import Project from "./Project";
-
+import TypewriterComponent from "typewriter-effect";
 const Body = () => {
   const [bounce, setBounce] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
       setBounce(false);
-    }, 3000);
+    }, 1000);
     }, []);
 
   return (
     <>
       <div className="bg-slate-300  md:pt-10 ">
         <div className="flex flex-col md:flex-row  items-center justify-center bg-slate-700  px-4 pt-16 md:pt-0 h-screen">
-          <div className={`text-center flex-wrap md:text-left md:ml-8 ${bounce ? 'animate-bounce ' : ''}`}>
+          <div className={`text-center flex-wrap md:text-left md:ml-8 ${bounce ? 'animate-spin' : ''}`}>
             <h1 className="font-bold text-4xl md:text-2xl text-zinc-300 mb-2">Hello,</h1>
             <h1 className="font-bold text-7xl md:text-4xl text-zinc-50 mb-2"> I'm <span className="text-yellow-500 animate-pulse">Satya Prakash</span></h1>
-            <h1 className="font-bold text-6xl md:text-3xl text-zinc-50 mb-4">Web Developer</h1>
+            <h1 className="font-bold text-6xl md:text-3xl text-zinc-50 mb-4"><span className="inline-block">
+
+            <TypewriterComponent
+                 options={{
+                 autoStart: true,
+                 loop: true,                  
+                 delay: 75,                   
+                 deleteSpeed: 50,           
+               }}
+             onInit={(typewriter) => {
+             typewriter
+            .typeString("Web Developer")
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString("Software Developer")
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString("Frontend Developer")
+            .pauseFor(1000)
+            .deleteAll()
+            .start();
+            }}
+            />
+
+            </span></h1>
             <p className="text-xl md:text-sm font-serif text-gray-400 max-w-lg py-4">   I am a React.js Developer and passionate about making user-friendly websites.
               <span className="text-xl md:text-sm font-serif text-gray-400 italic mt-2 block pt-2">  “Great web design without functionality is like a sports car with no engine.” </span>
             </p>   
